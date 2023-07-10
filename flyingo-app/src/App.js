a4692aef49f2b51dcd878e54e5cdde6dc86e6246
ArbajBegade <138779256+arbajbegade@users.noreply.github.com>
2023-07-10 20:00:14 +0530

    Update App.js

--- flyingo-app/src/App.js
+++ flyingo-app/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
+import Offer from './Components/Offer/Offer';
 
 function App() {
   return (
     <div className="bg-blue-200">
-     Hello! Flyingo 
+      <Navbar></Navbar>
+      <Offer></Offer>
+      <Contact></Contact>
+      <Footer></Footer>
+
     </div>
   );
 }
