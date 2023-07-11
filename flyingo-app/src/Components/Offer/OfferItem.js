eab5f97206b1359fdb66f0995f80aaf65d5dce0c
ArbajBegade <138779256+arbajbegade@users.noreply.github.com>
2023-07-12 00:16:07 +0530

    offer

---
+++ flyingo-app/src/Components/Offer/OfferItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function OfferItem({img, country, city}) {
+  return (
+    <div className='flex flex-col items-center shadow my-5 mx-10' >
+        <img src={img} alt="" className='woffer' />
+        <label className='font2 text-base '> {country} </label>
+        <label className='font1 text-xl font-semibold px-2 py-1 rounded bg-yellow-400'> {city} </label>
+        <label className='font4 text-xl mt-2 font-semibold textcolor pb-2'>Discover Your Self</label>
+    </div>
+  )
+}
+
+export default OfferItem
