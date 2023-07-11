9fbe8adcd44d2c69c505dea6405f9a28e0fe45dd
ArbajBegade <138779256+arbajbegade@users.noreply.github.com>
2023-07-11 18:07:59 +0530

    effects

--- flyingo-app/src/Components/Booking/BookBtn.js
+++ flyingo-app/src/Components/Booking/BookBtn.js
@@ -4,7 +4,7 @@ function BookBtn({btnname}) {
   return (
     <div className='flex space-x-2 justify-end mt-4 mr-8 items-center'>
         <label className='textcolor font-semibold'>+ Add Promocode</label>
-        <button className=' font-semibold px-6 py-3 bg-yellow-400 rounded-lg font1' >{btnname}</button>
+        <button className=' font-semibold px-6 py-3 bg-yellow-400 rounded-lg font1 effect' >{btnname}</button>
     </div>
   )
 }
