438457ea21235f0aba25ea53a561597e9c879d83
poojankadam <139098387+poojankadam@users.noreply.github.com>
2023-07-12 10:49:43 +0530

    tailwind

--- flyingo-app/src/Components/Offer/Offer.js
+++ flyingo-app/src/Components/Offer/Offer.js
@@ -6,7 +6,7 @@ import OfferList from './OfferList'
 function Offer() {
   return (
     <div className='flex flex-col items-center mt-20 '>
-      <label className='pb-4 font4 text-xl'>Featured destinations from India</label>
+      <label className='pb-4 font4 text-3xl'>Featured destinations from India</label>
       <OfferList></OfferList>
 
     </div>
--- flyingo-app/src/Components/Offer/OfferItem.js
+++ flyingo-app/src/Components/Offer/OfferItem.js
@@ -5,7 +5,7 @@ function OfferItem({img, country, city}) {
     <div className='flex flex-col items-center shadow my-5 mx-10' >
         <img src={img} alt="" className='woffer' />
         <label className='font2 text-base '> {country} </label>
-        <label className='font1 text-xl font-semibold px-2 py-1 rounded bg-yellow-400'> {city} </label>
+        <label className='font1 text-xl font-semibold px-2 py-1 rounded-2xl bg-yellow-400'> {city} </label>
         <label className='font4 text-xl mt-2 font-semibold textcolor pb-2'>Discover Your Self</label>
     </div>
   )
