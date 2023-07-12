02df3e0ef73740dd44d8de6cc36b65a40d0ddd41
ArbajBegade <138779256+arbajbegade@users.noreply.github.com>
2023-07-12 10:47:17 +0530

    c

--- flyingo-app/src/Components/Booking/Booking.js
+++ flyingo-app/src/Components/Booking/Booking.js
@@ -6,14 +6,14 @@ import BookBtn from './BookBtn'
 
 function Booking() {
   return (
-    <div className='width  -mt-20  m-auto shadow-2xl rounded-b-2xl pb-3'>
+    <div className='width  -mt-20  m-auto shadow-2xl rounded-b-2xl pb-2'>
         <div className='flex justify-evenly bgcolor rounded-t-2xl text-lg text-white font4 py-3'>
             <div>Book</div>
             <div>My Trip</div>
             <div>Check-In</div>
-            <div>Flight Statut</div>
+            <div>Flight Status</div>
         </div>
-        <div className='h-48 flex flex-col justify-center bg-white'>
+        <div className='h-48 flex flex-col justify-center bg-white pt-2'>
           <Book></Book>
           <BookBtn btnname="Show Flights"></BookBtn>
         </div>
