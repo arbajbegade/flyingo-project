f4faec47f31df8010c7973dfc30b5478a9ad83e7
poojankadam <139098387+poojankadam@users.noreply.github.com>
2023-08-04 18:45:53 +0530

    changes

--- flyingo-app/src/Routing/Explore/Explore.js
+++ flyingo-app/src/Routing/Explore/Explore.js
@@ -1,8 +1,17 @@
-import React from "react";
-import Carousel from "./Carousel";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DestinationDetails from "./DestinationDetails";
 
 function Explore() {
-  return <Carousel />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          {/* <Route path="/" element={<Home/>} /> */}
+          <Route path="/destination/:id" element={<DestinationDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Explore;
