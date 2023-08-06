27e2d9c4fc0eac81a56e8dca02d28529dabea518
poojankadam <139098387+poojankadam@users.noreply.github.com>
2023-08-06 09:55:30 +0530

    update routenavbar

--- flyingo-app/src/Routing/RoutNavbar.js
+++ flyingo-app/src/Routing/RoutNavbar.js
@@ -6,7 +6,7 @@ import Nav from './Nav'
 import Footer from '../Components/Footer/Footer'
 import Cargo from './Cargo/Cargo'
 import BookNow from './FastBook/BookNow'
-import Explore from './Explore/Explore'
+// import Explore from './Explore/Explore'
 import Carousel from './Explore/Carousel'
 import DestinationDetails from './Explore/DestinationDetails'
 
@@ -16,18 +16,8 @@ function RoutNavbar() {
         <Routes>
             <Route path='/' element={ <Nav/> } >
 <<<<<<< Updated upstream
-              <Route index element={ <div> 
-                <Offer/> 
-                <Carousel/> 
-                <Contact/> 
-                <Footer/> 
-                </div> } />
-              {/* <Route path='/explore' element={<Explore/>} /> */}
+              <Route index element={ <div> <Offer/> <Carousel/> <Contact/> <Footer/> </div> } />
               <Route path="/destination/:id" element={<DestinationDetails />} />
-=======
-              <Route index element={ <div> <Offer/> <Contact/> <Footer/> </div> } />
-              <Route path='/explore' element={<Explore/>} />
->>>>>>> Stashed changes
               <Route path='/cargo' element={<Cargo/>} />
             </Route>
             <Route path='/booknow' element={ <BookNow/> } />
