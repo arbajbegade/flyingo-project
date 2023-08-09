caec29f263e9230cff0d610f09682e3b102d8d3f
ArbajBegade <138779256+arbajbegade@users.noreply.github.com>
2023-08-09 22:19:38 +0530

    a

--- flyingo-app/src/Routing/Nav.js
+++ flyingo-app/src/Routing/Nav.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Booking from "../Components/Booking/Booking";
 import Navbar from '../Components/Navbar/Navbar'
-import TripSection from '../Components/Trip/TripSection'
 
 
 function Main() {
@@ -10,7 +9,6 @@ function Main() {
     <div>
       <Navbar/>
       <Booking/>
-      <TripSection/>
       <Outlet/>
     </div>
   )
