db1784ea32e2c235d07760e8e8131e0b6f84b33c
Priti Jadhav <pritijadhav1210@gmail.com>
2023-08-09 19:17:09 +0530

    contact

--- flyingo-app/src/Components/FeedBackAndReview/FeedbackPage.js
+++ flyingo-app/src/Components/FeedBackAndReview/FeedbackPage.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import FeedbackForm from './FeedBackForm';
-
-// const FeedbackPage = () => {
-//     return (
-//     <div className='flex flex-col items-center bgfdback py-8 px-4 shadow my-5 mx-5  mb-10 border-style: solid;'>
-
-//       <h1 className="text-3xl font-semibold text-center mb-4 text-pink-950 font5">Feedback and Rating</h1>
-//       <h2 className="text-xl font-semibold text-center mb-8 text-pink-800">
-//       Tell us about your experience with Airway and rate it accordingly.
-//       </h2>
-//       <FeedbackForm/>
-//     </div>
-    
-//   );
-// };
-
-// export default FeedbackPage;
 import React from 'react';
 import FeedbackForm from './FeedbackForm';
 
