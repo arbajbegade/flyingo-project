bdb1bbe578c931007b825adde3cf0e7ff479f25a
poojankadam <139098387+poojankadam@users.noreply.github.com>
2023-08-10 19:05:55 +0530

    changes

--- flyingo-app/src/Components/Offer/OfferDetails.js
+++ flyingo-app/src/Components/Offer/OfferDetails.js
@@ -40,7 +40,6 @@
 
 // export default OfferDetails;
 
-
 // import React from "react";
 // import { useParams } from "react-router-dom";
 // import offers from "./offers";
@@ -90,23 +89,31 @@ function OfferDetails() {
   return (
     <div className="bg-gray-100 min-h-screen py-16">
       <div className="max-w-3xl mx-auto p-6 bg-white shadow-lg rounded-lg">
-        <div className="text-2xl font-semibold mb-4 text-gray-800">{offer.title}</div>
+        <div className="text-2xl font-semibold mb-4 text-gray-800">
+          {offer.title}
+        </div>
 
         <div className="bg-gray-200 p-4 rounded-lg mb-4">
           <div className="text-lg font-semibold text-gray-700">Highlight</div>
 
           <div className="flex justify-center items-center mt-2 space-x-4">
             <span className="text-green-500 font-semibold">{offer.coupon}</span>
-            <span className="text-blue-500 font-semibold">{offer.discount}</span>
+            <span className="text-blue-500 font-semibold">
+              {offer.discount}
+            </span>
             <span className="text-gray-500">{offer.validity}</span>
           </div>
         </div>
 
         <div>
-          <div className="text-lg font-semibold mb-2 text-gray-700">About this offer:</div>
+          <div className="text-lg font-semibold mb-2 text-gray-700">
+            About this offer:
+          </div>
           <ul className="list-disc pl-6 space-y-2">
             {offer.about.map((x, index) => (
-              <li key={index} className="text-gray-600">{x}</li>
+              <li key={index} className="text-gray-600">
+                {x}
+              </li>
             ))}
           </ul>
         </div>
