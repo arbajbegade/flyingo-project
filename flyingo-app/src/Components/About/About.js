4726452432f1dba183e2aa1bbc00dcb47ac170ab
poojankadam <139098387+poojankadam@users.noreply.github.com>
2023-08-17 11:31:19 +0530

    update

--- flyingo-app/src/Components/About/About.js
+++ flyingo-app/src/Components/About/About.js
@@ -25,7 +25,8 @@ export default function About() {
               <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                 <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                   <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                    We're changing the way people connect.
+                    Pint aliqua anim nulla sunt mollit id pariatur in voluptate
+                    cillum.
                   </h1>
                   <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
                     Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in
--- flyingo-app/src/Components/About/team.js
+++ flyingo-app/src/Components/About/team.js
@@ -5,6 +5,6 @@ const team = [
     imageUrl:
       " /photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
   },
-  // More people...
+  // Add more people...
 ];
 export default team;
